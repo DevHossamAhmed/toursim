4a7b623dff683c31f881a89c4d0f34a1b4f8d8f7
DevHossamAhmed <dev.hossamelden@gmail.com>
2024-08-28 12:47:21 +0300

    update

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import Container from "../Container";
-import Image from "next/image";
-import picLogo from "../../assets/images/logo.png";
+// import Image from "next/image";
+// import picLogo from "../../assets/images/logo.png";
 
 const About = () => {
   return (
     <div id="about" className="w-full bg-[#013032] text-white">
       <Container>
         <div className="flex flex-col lg:flex-row lg:justify-between px-36 py-9">
-          <div className="w-full flex justify-center lg:justify-start animate-fadeInSlideIn">
+          {/* <div className="w-full flex justify-center lg:justify-start animate-fadeInSlideIn">
             <img
               src="https://upload.wikimedia.org/wikipedia/ar/c/ca/%D8%B4%D8%B9%D8%A7%D8%B1_%D9%88%D8%B2%D8%A7%D8%B1%D8%A9_%D8%A7%D9%84%D8%B3%D9%8A%D8%A7%D8%AD%D8%A9_%D8%A7%D9%84%D8%B3%D8%B9%D9%88%D8%AF%D9%8A%D8%A9.svg"
               alt="شعار وزارة السياحة السعودية"
               className="w-[180px] sm:w-[200px] h-auto mb-4 md:mb-0 m-auto md:m-0"
             />{" "}
-          </div>
+          </div> */}
           <div className="w-full mt-[24px] lg:mt-0 animate-fadeInSlideIn">
             <h2 className="text-[40px] text-[#82D9Bf] font-bold">نبذة</h2>
             <p className="text-[20px] font-medium leading-[30px]">
